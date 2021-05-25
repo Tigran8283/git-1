7203447d9f39e52c93619ad25bea89bb2148a5f5
Tigran8283 <tigran.kocharyan@orders.co>
2021-05-25 16:54:05 +0400

    homework

---
+++ index.js
@@ -0,0 +1,17 @@
+var text1="hello"
+var text2='barev'
+
+var a=8
+var b=9
+
+var sum=a+b
+var mult=a*b
+var sub=a-b
+console.log("5.", sum)
+console.log('6.',mult)
+console.log('7.',sub)
+console.log('8.',sub+mult)
+console.log('9',text1+text2)
+console.log('10',text1+mult)
+
+
