2869ce6404e8a5a0a3a563e1a31ae46c7dd52697
Tigran8283 <tigran.kocharyan@orders.co>
2021-05-27 16:59:05 +0400

    tnayin33

---
+++ tnayin33.js
@@ -0,0 +1,132 @@
+ /* var b=prompt("mutqagrel dzer anun@");
+                        alert("barev "+b); */
+
+
+ /* var day=365;
+ var c="erkir";
+ var m=900000000;
+ var s="arev";
+
+ document.write("Մենք ապրում ենք "+c+" մոլորակում, որը պտտվում է "+s+" շուրջը "+day+" օրում: Մեր մոլորակում մարդկանց թիվը մոտ ապագայում կհասնի մոտավորապես "+m+"-ի:");
+  */
+
+
+ /* 
+   var a=+prompt();
+  var b=a % 10;
+  var c=((a-b)/10)%10;
+  var d=parseInt(a/100)
+  console.log(b+c+d);
+  */
+
+ //  console.log(Math.max(1,-1,5,8,14));
+ // console.log(Math.min(1,-1,5,8,14));
+
+ // console.log(Math.abs(-5));
+ // console.log(Math.pow(2,3));
+ // console.log(Math.sqrt(9));
+ // console.log(Math.floor(4.9));
+ // console.log(Math.ceil(4.1));
+ // console.log(Math.round(4.4));
+ // console.log(Math.random());
+ // console.log(Math.random()*10);
+ // console.log(Math.random()*10+5);
+
+ // 
+
+
+ // var a=7;
+ // switch(a){
+ //     case 8:
+ //             console.log("a=8");
+ //             break;
+ //     case 50:
+ //             console.log("a=50");
+ //             break;
+ //     case 7:
+ //             console.log("a=7");
+ //             break;
+ // }
+
+
+ // var a=7;
+ //  switch(a){
+ //    case 8:            
+ //    console.log("a=8");
+ //             break;
+ //     case 50:
+ //             console.log("a=50");
+ //             break;
+ //     case 70:
+ //             console.log("a=70");
+ //             break;
+ //             default:
+ //             console.log("sxal tiv");
+ //             break;
+ // }
+
+
+
+ // var a=7;
+ // switch (a){
+ //     case 7:
+ //     console.log("7");
+ //     case 8:
+ //     console.log("8");
+ // }
+
+ /* var a=+prompt()
+ switch (a){
+     case 5:
+     case 6:
+             console.log("5 kam 6");
+             break;
+             case 11:
+      case 12:
+             console.log("11 kam 12");
+             break;
+ }  */
+
+ // var a=+prompt();
+ // switch (a){
+ //     case 10: 
+ //     case 12:
+ //     case 14:
+ //         console.log("10 kam 12 kam 14");break;
+
+ //     case 16:
+ //     case 20:
+ //         console.log("16 @ kam 20 @");
+ // }
+
+ // var a=-10
+ // var b=-20
+ // console.log(Math.abs(a+b));
+ //  console.log(Math.random()*90 +10);
+
+ // var a=+prompt("տարիք")
+ // if(a<18){
+ //     alert( " “Այս կայքում Ձեր մուտքը թույլատրված չէ”,")
+ // }else{
+ //    alert("հաջողությամբ հաստատվել է”։")
+ // }
+
+
+ var a = +prompt()
+ switch (a) {
+     case 0:
+     case 1:
+     case 11:
+         console.log("Ձմեռ");
+         break;
+
+     case 2:
+     case 3:
+     case 4:
+         console.log("Գարուն");
+     case 5:
+     case 6:
+     case 7:
+         console.log("“Աշուն")
+
+ }
